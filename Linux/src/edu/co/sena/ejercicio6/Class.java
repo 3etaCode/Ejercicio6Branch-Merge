7e3ebc983779550e4e980989c8a61e20977f6691
PeterD <pdglobo@misena.educ.co>
2016-02-20 17:55:56 +0100

    Este es el commit de Linux v 0.1

---
+++ Linux/src/edu/co/sena/ejercicio6/Class.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.ejercicio6;
+
+/**
+ *
+ * @author PeterD
+ */
+public class Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Linux v 0.1");
+    }
+    
+}
